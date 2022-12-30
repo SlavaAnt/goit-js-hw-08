41d35759c2e9f36bad6933884b8cac716c7bbe36
Slava Antolyk <Sl.antolyk@gmail.com>
2022-12-30 19:49:09 +0200

    libraries

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+test = 10;
+console.log(test);
